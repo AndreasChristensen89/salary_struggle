35bb2fcd3e153751b0aee9016f412a31f63283a4
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-10 11:23:38 +0000

    Add description to items and interviewers, adjust details page

--- codex/models.py
+++ codex/models.py
@@ -20,6 +20,7 @@ class Interviewer(models.Model):
     image = models.ImageField(null=False, blank=False)
     paid = models.BooleanField(default=False)
     level = models.IntegerField(default=InterviewerLevel.LOW, choices=InterviewerLevel.choices)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.friendly_name
@@ -36,6 +37,7 @@ class Item(models.Model):
     coding = models.IntegerField(default=0)
     energy = models.IntegerField(default=0)
     image = models.ImageField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
