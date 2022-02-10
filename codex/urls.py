f7dfd84f108209b89d2bbb8d01c758c07068530e
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-10 11:12:24 +0000

    Add item and interviewer detail pages

--- codex/urls.py
+++ codex/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'codex'
 
 urlpatterns = [
-    path('', views.full_codex, name='full_codex'),
+    path('items/', views.items_index, name='items'),
+    path('items/<item_id>', views.item_details, name='item_details'),
+    path('interviewers/', views.character_index, name='interviewers'),
+    path('interviewers/<interviewer_id>', views.interviewer_details, name='interviewer_details'),
 ]
--- codex/views.py
+++ codex/views.py
@@ -1,16 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Interviewer, Item
 
 
-def full_codex(request):
-    """ A view to return the codex page """
+def items_index(request):
+    """ A view to return the items page """
 
-    interviewers = Interviewer.objects.all()
     items = Item.objects.all()
 
     context = {
-        'interviewers': interviewers,
         'items': items,
     }
 
-    return render(request, 'codex/codex.html', context)
+    return render(request, 'codex/items.html', context)
+
+
+def character_index(request):
+    """ A view to return the character page """
+
+    interviewers = Interviewer.objects.all()
+
+    context = {
+        'interviewers': interviewers,
+    }
+
+    return render(request, 'codex/interviewers.html', context)
+
+
+def item_details(request, item_id):
+    """ A view to return the item details page """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'codex/item_details.html', context)
+
+
+def interviewer_details(request, interviewer_id):
+    """ A view to return the interviewer details page """
+
+    interviewer = get_object_or_404(Interviewer, pk=interviewer_id)
+
+    context = {
+        'interviewer': interviewer,
+    }
+
+    return render(request, 'codex/interviewer_details.html', context)
