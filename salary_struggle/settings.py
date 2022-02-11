a754c13f55a80048ab401bb66ee7c3a242ef96c1
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-11 11:42:41 +0000

    Add shopping bag context and delivery logic

--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',   # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'shopping_bag.contexts.shopping_bag_contents',
             ],
         },
     },
@@ -163,3 +164,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
