63efbef724179e238acee67a5b4c8db3932ddcd9
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-14 14:44:57 +0000

    Added toasts

--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -85,6 +85,9 @@ TEMPLATES = [
     },
 ]
 
+# Tells it to store messages in the session
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from shop.models import Product
 
 
 def view_shopping_bag(request):
@@ -10,6 +13,7 @@ def view_shopping_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))    # convert since it comes as a string from the template
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})    # We check to see if there's a bag var, if not we create one {}
@@ -19,6 +23,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity     # increment the quantity
     else:
         bag[item_id] = quantity  # create a key of the product's id and set it equal to the quatity
+        messages.success(request, f'{product.name} was added to the shopping bag')
 
     request.session['bag'] = bag    # put the bag into the session, override the variable with an update version
     return redirect(redirect_url)
