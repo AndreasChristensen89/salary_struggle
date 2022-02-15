0c05e4c0202346171daee95f4ded9c5ec6f67586
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-15 09:57:27 +0000

    Created Order class and OrderItem class

--- premium/models.py
+++ premium/models.py
@@ -1,3 +1,83 @@
-from django.db import models
+import uuid     # to generate order number
 
-# Create your models here.
+from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from shop.models import Product
+
+
+class Order(models.Model):
+    """
+    Class for instances of orders
+    Linked to OrderItem class, which handles totals
+    """
+    
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Private method, generates a unique order number
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Updates grand total when item is added, accounting for delivery costs.
+        Uses Sum() across all items
+        """
+        self.order_total = self.orderitems.aggregate(Sum('item_total'))['item_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides save method to set the order number,
+        if not set already, bu using method above
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderItem(models.Model):
+    """ 
+    Class that iterates through every item from an order instance,
+    attaches it to the order, and updates the delivery costs and total
+    """
+    
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='orderitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides save method to set the item_total, 
+        and update the order_total in the Order class
+        """
+        self.item_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
--- shop/models.py
+++ shop/models.py
@@ -1,13 +1,27 @@
 from django.db import models
+import random
+import string
 
 
 class Product(models.Model):
     """ Model for products"""
 
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides save method to set alphanumeric sku
+        Generate code taken from stackover:
+        https://stackoverflow.com/questions/2511222/efficiently-generate-a-16-character-alphanumeric-string
+        """
+        if not self.sku:
+            sku = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
+            self.sku = sku
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.name}'
