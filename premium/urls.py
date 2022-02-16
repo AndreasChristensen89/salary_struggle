c6b2d3579882f42b0e4b7958e517b16805046752
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-16 17:46:58 +0000

    Add webhooks.py, export signing secret

--- premium/urls.py
+++ premium/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 app_name = 'premium'
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('webhook/', webhook, name='webhook'),
     path('premium/', views.premium, name='premium'),
 ]
--- premium/webhook_handler.py
+++ premium/webhook_handler.py
@@ -12,5 +12,21 @@ class StripeWHHandler:
         Handles webhook events
         """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handles the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handles the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
