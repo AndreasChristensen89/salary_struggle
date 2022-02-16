741845fab1f02b27b64ee8811710ebf1d34d46b1
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-16 09:52:07 +0000

    Add stripe elements

--- premium/views.py
+++ premium/views.py
@@ -15,13 +15,15 @@ def checkout(request):
 
     bag = request.session.get('bag', {})
     if not bag:
-        message.error(request, "The bag is currently empty")
+        messages.error(request, "The bag is currently empty")
         return redirect(reverse('shop:all_products'))
 
     order_form = OrderForm()
     template = 'premium/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KTjPeDGj8gZbV1S5AervqzGY60YLX252uD0RwTaSJmc1uCfiS7PoiFPz3VzwLMA5ylSftvQZfdniZLAbx61uDli00Xg4O9ybe',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
