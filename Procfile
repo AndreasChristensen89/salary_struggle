d6acccc4ec508ad8bc8673e1976f3daa38695c5a
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-21 09:58:56 +0000

    Deployment part two

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn salary_struggle.wsgi:application
--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-69jcc2*ys@-2^4ycgm!1sp#kp=ps#rc=m=w_&-0b88)4#4-&z%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['salary_struggle.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'salary_struggle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
