cb8d1faa3d6f5e847d343de94400d4fa28b2a807
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-21 10:10:47 +0000

    Remove secret key and set debug

--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69jcc2*ys@-2^4ycgm!1sp#kp=ps#rc=m=w_&-0b88)4#4-&z%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['salary-struggle.herokuapp.com', 'localhost']
 
