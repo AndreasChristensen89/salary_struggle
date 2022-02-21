d31db54e8476fa9f6c59d273035a543427a533f5
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-21 13:04:40 +0000

    Change spelling mistake in access key variable

--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -185,8 +185,8 @@ if 'USE_AWS' in os.environ:
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'salary-struggle'
     AWS_S3_REGION_NAME = 'eu-west-3'
-    AWS_ACCCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
