9b7dd25b4e1860ddd1e8572431cdca81b4cc6c0d
AndreasChristensen89 <brogaard89@gmail.com>
2022-02-23 11:46:28 +0000

    Change navbar for game, remove hamburger js

--- profiles/models.py
+++ profiles/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
-from codex.models import Item
 
 
 class Profile(models.Model):
