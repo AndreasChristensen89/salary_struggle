c3b8c0ba110b25d7323e58dac8c8b029370b8ec8
AndreasChristensen89 <brogaard89@gmail.com>
2022-03-05 13:50:56 +0000

    Change game nav bar

--- grind/static/grind/js/overlay.js
+++ grind/static/grind/js/overlay.js
@@ -2,7 +2,5 @@ document.addEventListener('DOMContentLoaded', function () {
     $("#sleep").click(function(){
         $('.overview').fadeToggle(100);
         $('#loading-overlay').fadeToggle(100);
-        window.location.href = '/some/new/page';
-        return false;
     });
 });
