f4539bab9eb905b2595d443f42ea78d301b2119b
AndreasChristensen89 <brogaard89@gmail.com>
2022-03-23 08:14:57 +0000

    Change hamburger js to jquery syntax

--- grind/static/grind/js/hamburger.js
+++ grind/static/grind/js/hamburger.js
@@ -1,9 +1,6 @@
 let menu = document.querySelector(".menu");
-let hamburger = document.querySelector(".hamburger");
-let xIcon = document.querySelector(".xIcon");
-let menuIcon = document.querySelector(".menuIcon");
 
-hamburger.addEventListener("click", moveMenu);
+$('.hamburger').click(moveMenu);
 
 function moveMenu() {
     if (menu.classList.contains("menuDown")) {
@@ -12,14 +9,16 @@ function moveMenu() {
         $(".menu").attr('id', 'menu-bg');
         $(".xIcon").removeClass("d-block");
         $(".xIcon").addClass("d-none");
-        menuIcon.style.display = "block";
+        $(".menuIcon").addClass("d-block");
+        $(".menuIcon").removeClass("d-none");
     } else {
         menu.classList.add("menuDown");
         $(".menuLink").removeClass("d-none");
         $(".menu").removeAttr('id');
         $(".xIcon").removeClass("d-none");
         $(".xIcon").addClass("d-block");
-        menuIcon.style.display = "none";
+        $(".menuIcon").addClass("d-none");
+        $(".menuIcon").removeClass("d-block");
     }
 }
 
