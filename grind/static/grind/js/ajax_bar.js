ff77bfff4e12fe272546586bce54e71f92b3228c
AndreasChristensen89 <brogaard89@gmail.com>
2022-03-24 09:28:03 +0000

    Fix bar actions, add proper loading-overlays

--- grind/static/grind/js/ajax_bar.js
+++ grind/static/grind/js/ajax_bar.js
@@ -59,23 +59,29 @@ $("#converse").click(function() {
     },
     success: function(){
         if (energy >= (40-endurance) && randomNumber <= 2) {
-            $("#converse").addClass("bg-success");
             $("#charm").text(charm+2);
             $("#energy").html(`<i class="fas fa-bolt mx-1"></i> ${energy-(40-endurance)}`);
                 
             charm = charm+2;
             energy = energy-(40-endurance);
 
+            $('.overview').fadeToggle(100);
+            $(".loading-overlay").eq(0).fadeToggle(100);
+            $('#drink-pen').addClass('d-none');
+
             setTimeout(() => { 
-                $("#converse").removeClass("bg-success");
-            }, 800);
+                $('.overview').fadeToggle(100);
+                $(".loading-overlay").eq(0).fadeToggle(100);
+                $('#drink-pen').removeClass('d-none');
+            }, 2500);
         } else {
-            $("#converse").addClass("bg-danger");
-            $("#energy").text(energy-(40-endurance));
-            energy = energy-(40-endurance);
+            $('.overview').fadeToggle(100);
+            $(".loading-overlay").eq(1).fadeToggle(100);
+
             setTimeout(() => { 
-                $("#converse").removeClass("bg-danger");
-            }, 800);
+                $('.overview').fadeToggle(100);
+                $(".loading-overlay").eq(1).fadeToggle(100);
+            }, 2500);
         }
             
     }
