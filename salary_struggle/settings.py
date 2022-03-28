9b9484089de2f8aeae70259cb7f22bafc82225de
AndreasChristensen89 <brogaard89@gmail.com>
2022-03-28 13:55:34 +0000

    Change debug + admin item to original

--- salary_struggle/settings.py
+++ salary_struggle/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = False
 
 ALLOWED_HOSTS = ['salary-struggle.herokuapp.com', 'localhost']
 
